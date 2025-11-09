c31329bb8a422011be0a47aa6a8ed54641150c33
samuelbinogma <sbinogma41@gmail.com>
2025-11-09 08:39:44 +0000

    create a new react app and add some inline styles

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{ padding: '3rem', backgroundColor: 'goldenrod'}}>
+            <p style={{ color: 'green', fontWeight: 'bold', fontSize: '1.5rem'}}>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ border: '2px solid white'}}>
+            <p style={{ color: 'gold', background: 'green'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
