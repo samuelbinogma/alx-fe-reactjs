08bb071d68397756b9a8d8ca7a5dc27589cdbd3a
samuelbinogma <sbinogma41@gmail.com>
2025-11-30 12:43:35 +0000

    changes in container styles

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,13 +4,13 @@ function UserProfile() {
         className="user-profile
                 bg-gray-100 
                 sm:p-4 md:p-8 
-                max-w-xs sm:max-w-sm 
+                max-w-xs md:max-w-sm sm:max-w-sm 
                 mx-auto my-20 rounded-lg shadow-lg">
 
       <img src="https://via.placeholder.com/150" alt="User" 
       className="w-24 h-24 sm:w-36 sm:h-36 rounded-full mx-auto" />
 
-      <h1 className="text-lg sm:text=xl
+      <h1 className="text-lg md:text-xl
                     text-blue-800 
                     my-4 text-center
                     font-semibold">
