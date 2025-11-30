c8d1a3791d942810a65d1d5ef45f77a116ea3d3a
samuelbinogma <sbinogma41@gmail.com>
2025-11-30 12:46:35 +0000

    modified tailwindcss styles in img tag

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -8,7 +8,7 @@ function UserProfile() {
                 mx-auto my-20 rounded-lg shadow-lg">
 
       <img src="https://via.placeholder.com/150" alt="User" 
-      className="w-24 h-24 sm:w-36 sm:h-36 rounded-full mx-auto" />
+      className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto" />
 
       <h1 className="text-lg md:text-xl
                     text-blue-800 
