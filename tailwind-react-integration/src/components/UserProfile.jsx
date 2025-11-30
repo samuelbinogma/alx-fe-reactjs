1971c350616a79e924e3fc3e244d190f68dc1a47
samuelbinogma <sbinogma41@gmail.com>
2025-11-30 12:31:08 +0000

    creating responsive design with tailwindcss

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,25 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div 
+        className="user-profile
+                bg-gray-100 
+                p-4 sm:pm-8 
+                max-w-xs sm:max-w-sm 
+                mx-auto my-20 rounded-lg shadow-lg">
+
       <img src="https://via.placeholder.com/150" alt="User" 
-      className="w-36 h-36 rounded-full mx-auto" />
-      <h1 className="text-xl text-blue-800 my-4 text-center">John Doe</h1>
-      <p className="text-base text-gray-600 text-center">
+      className="w-24 h-24 sm:w-36 sm:h-36 rounded-full mx-auto" />
+
+      <h1 className="text-lg sm:text=xl
+                    text-blue-800 
+                    my-4 text-center
+                    font-semibold">
+                    John Doe
+        </h1>
+      <p className="text-sm sm:text-base 
+                text-gray-600 
+                text-center
+                leading-relaxed">
         Developer at Example Co. Loves to write code and explore new technologies.
         </p>
     </div>
