422333ee227721651ae5450df09d8d2095eb41c8
samuelbinogma <sbinogma41@gmail.com>
2025-11-30 12:53:08 +0000

    add advance interactivity and transitions with tailwindcss

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -5,15 +5,23 @@ function UserProfile() {
                 bg-gray-100 
                 sm:p-4 md:p-8 
                 max-w-xs md:max-w-sm sm:max-w-sm 
-                mx-auto my-20 rounded-lg shadow-lg">
+                mx-auto my-20 rounded-lg shadow-lg
+                hover:shadow-xl 
+                transition-shadow duration-300 ease-in-out
+                ">
 
       <img src="https://via.placeholder.com/150" alt="User" 
-      className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto" />
+      className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto
+                hover:scale-110 
+                transition-transform duration-300 ease-in-out 
+                cursor-pointer"/>
 
       <h1 className="text-lg md:text-xl
                     text-blue-800 
                     my-4 text-center
-                    font-semibold">
+                    font-semibold
+                    hover:text-blue-500
+                    transition-colors duration-300">
                     John Doe
         </h1>
       <p className="text-sm sm:text-base 
