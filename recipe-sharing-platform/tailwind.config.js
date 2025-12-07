1bebad9cb97c9917d69afdee9aaef941ff1ac023
samuelbinogma <sbinogma41@gmail.com>
2025-12-07 10:14:08 +0000

    create a recipe sharing platform using react and tailwindcss

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
