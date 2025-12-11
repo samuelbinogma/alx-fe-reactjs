2d6145556dc1dc44e2acad9d2f8cac9b21058413
samuelbinogma <sbinogma41@gmail.com>
2025-12-11 20:17:13 +0000

    create a user registration form using controlled components

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,16 @@
+import './index.css'
+import './App.css'
+import React from 'react';
+import RegistrationForm from './components/RegistrationForm';
+
+
+function App() {
+
+  return (
+    <div>
+      <RegistrationForm />
+    </div>
+  )
+}
+
+export default App
